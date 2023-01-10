23063a4db43c0536306c6618f32ffe834af888f3
Daffa Raka <daffarakals@gmail.com>
2023-01-10 12:27:09 +0700

    First Commit

---
+++ resources/views/homepage/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="cotnainer-fluid bg-dark" style="min-height: 10vh;">
+    <div class="container">
+        <div class="row">
+
+        </div>
+        <div class="d-flex justify-content-center py-4">
+            <h6 class="text-light"> &#169; 2021 Backtify , All rights reserved</h6>
+        </div>
+    </div>
+</div>
---
+++ resources/views/homepage/partials/nav-home.blade.php
@@ -0,0 +1,77 @@
+<nav class="navbar navbar-dark bg-dark navbar-expand-lg bg-light">
+    <div class="container py-2">
+        <a class="navbar-brand" href="{{route('homepage')}}">Backtify</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                {{-- <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Link
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Link</a>
+                </li> --}}
+            </ul>
+            <div class="d-flex">
+                <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a href="{{route('homepage.profile')}}" class="dropdown-item">Profile</a>
+                                <a href="{{route('dashboard')}}" class="dropdown-item {{Auth::user()->role == 'Admin' ? '' : 'd-none' }}">Dashboard (Admin only)</a>
+                                <a href="{{route('homepage.cart')}}" class="dropdown-item">Cart</a>
+                                <a href="{{route('homepage.transaction')}}" class="dropdown-item">Transaction</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
